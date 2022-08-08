e11edefd74b3c3ecbc0e610d867737072fa72d1c
dbores3 <davidborespaoli@gmail.com>
2022-08-07 20:39:04 -0500

    Further testing

--- src/turbines/turbines.controller.spec.ts
+++ src/turbines/turbines.controller.spec.ts
@@ -1,21 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TurbinesController } from './turbines.controller';
 import { TurbinesService } from './turbines.service';
+import { FileDto } from './file.dto';
+import { TurbinesDto } from './turbines.dto';
 
 describe('TurbinesController', () => {
   let controller: TurbinesController;
+  let service: TurbinesService;
 
   beforeEach(async () => {
+    const ApiServiceProvider = {
+      provide: TurbinesService,
+      useFactory: () => ({
+        readFile: jest.fn(() => []),
+        queryData: jest.fn(() => [])
+      })
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TurbinesController],
-      providers: [TurbinesService],
+      providers: [TurbinesService, ApiServiceProvider],
       exports: [TurbinesService],
     }).compile();
 
     controller = module.get<TurbinesController>(TurbinesController);
+    service = module.get<TurbinesService>(TurbinesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe("readFile",() => {
+    it("should call readFile method", () => {
+      const dto = new FileDto();
+      expect(controller.readFile(dto)).not.toEqual(null);
+    })
+
+    it("should call readFile method", () => {
+      const dto = new FileDto();
+      dto.file = "example_indicator.csv";
+      controller.readFile(dto);
+      expect(service.readFile).toHaveBeenCalled();
+      expect(service.readFile).toHaveBeenCalledWith(dto);
+    })
+  })
+
+  describe("queryData",() => {
+    it("should call queryData method", () => {
+      const dto = new TurbinesDto();
+      expect(controller.queryData(dto)).not.toEqual(null);
+    })
+
+    it("should call queryData method", () => {
+      const dto = new TurbinesDto();
+      controller.queryData(dto);
+      expect(service.queryData).toHaveBeenCalled();
+      expect(service.queryData).toHaveBeenCalledWith(dto);
+    })
+  })
 });
--- src/turbines/turbines.service.spec.ts
+++ src/turbines/turbines.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TurbinesService } from './turbines.service';
+import { FileDto } from './file.dto';
+import { TurbinesDto } from './turbines.dto';
 
 describe('TurbinesService', () => {
   let service: TurbinesService;
 
   beforeEach(async () => {
+    const ApiServiceProvider = {
+      provide: TurbinesService,
+      useFactory: () => ({
+        readFile: jest.fn(() => []),
+        queryData: jest.fn(() => [])
+      })
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TurbinesService],
+      providers: [TurbinesService, ApiServiceProvider],
     }).compile();
 
     service = module.get<TurbinesService>(TurbinesService);
@@ -14,5 +24,24 @@ describe('TurbinesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe("readFile",() => {
+    it('should call readFile method with expected params', async () => {
+      const readFileSpy = jest.spyOn(service, 'readFile');
+      const dto = new FileDto();
+      dto.file = "example_indicator.csv";
+      service.readFile(dto);
+      expect(readFileSpy).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe("queryData",() => {
+    it('should call queryData method with expected params', async () => {
+      const queryDataSpy = jest.spyOn(service, 'queryData');
+      const dto = new TurbinesDto();
+      service.queryData(dto);
+      expect(queryDataSpy).toHaveBeenCalledWith(dto);
+    });
   });
 });
